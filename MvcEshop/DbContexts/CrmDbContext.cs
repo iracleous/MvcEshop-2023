1286a372eccf9b00efd995077792708334ba07d9
dimitris <iracleous@gmail.com>
2023-07-04 20:04:36 +0300

    Add project files.

---
+++ MvcEshop/DbContexts/CrmDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols;
+using SqlAzureConnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlAzureConnect.DbContexts
+{
+    public class CrmDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        public CrmDbContext() { }
+        public CrmDbContext(DbContextOptions<CrmDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
